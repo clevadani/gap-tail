3dc01e138d7439daeba31f373cd7ff37205bf57e
Daniel <clevadani@gmail.com>
2024-12-15 19:12:49 +0000

    WIP: Tailwind setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+
+app.use(morgan('combined'));
+app.use(cors());
 
 app.use(express.static('public'));
 
